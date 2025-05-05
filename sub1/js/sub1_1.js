f0ce64661c98fed72d557529abac62a991c183fc
yis <https://github.com/yis716/rgb_project.git>
2025-05-05 19:51:53 +0900

    sub1_1 작업진행 html완료

---
+++ sub1/js/sub1_1.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const lines = document.querySelectorAll('.move');
+
+//     const observer = new IntersectionObserver((entries) => {
+//     if (entries[0].isIntersecting) {
+//         lines.forEach(line => line.classList.add('show'));
+//         observer.disconnect();
+//     }
+//     }, {
+//     threshold: 0.4
+//     });
+
+//     observer.observe(document.querySelector('.moveLine'));
+// });
