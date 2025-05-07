cca80376dfe6cf74077aca590e4725618312ebd1
yis <https://github.com/yis716/rgb_project.git>
2025-05-07 20:14:27 +0900

    전체적인 수정완료 및 php작업중

--- common/js/common.js
+++ common/js/common.js
@@ -74,46 +74,24 @@ var on_off=false;  //false(안오버)  true(오버)
     });
 
 //top으로 이동
-$(window).on('scroll',function(){ //스크롤 값의 변화가 생기면
-            var scroll = $(window).scrollTop(); //스크롤의 거리
-            
-            
-            $('.text').text(scroll);
-
-            if(scroll>=500){ //300이상의 거리가 발생되면
-                $('.top_move').fadeIn('slow');  //top보임
-            }else{
-                $('.top_move').fadeOut('fast');//top안보임
-            }
-        });
+// $(window).on('scroll',function(){ //스크롤 값의 변화가 생기면
+//     var scroll = $(window).scrollTop(); //스크롤의 거리
     
-        $('.top_move').click(function(e){
-            e.preventDefault();
-            //상단으로 스르륵 이동합니다.
-            $("html,body").stop().animate({"scrollTop":0},1000); //요거 중요 {"scrollTop":100 -> (top좌표만큼 이동)},1000
-        });
-
-        //슬라이드 메뉴 클릭시 해당 콘텐츠로 스스륵~~~ 이동
-        // $('.slideMenu a').click(function(e){
-        //     e.preventDefault(); //href="#" 속성을 막아주는..메소드
     
-        //     var value=0; //이동할 스크롤의 거리
+//     $('.text').text(scroll);
 
-        //     if($(this).hasClass('link1')){   //첫번째 메뉴를 클릭했냐?   if($(this).is('#link1')){
-        //         value= $('#content .con1').offset().top;  // .offset == 해당 콘테츠의 상단의 거리~~ left/top/right/bottom
-        //     }else if($(this).hasClass('link2')){
-        //         value= $('#content .con2').offset().top; 
-        //     }else if($(this).hasClass('link3')){
-        //         value= $('#content .con3').offset().top; 
-        //     }else if($(this).hasClass('link4')){
-        //         value= $('#content .con4').offset().top; 
-        //     }
-            
-        // $("html,body").stop().animate({"scrollTop":value},1000);
-        // });
+//     if(scroll>=700){ //700이상의 거리가 발생되면
+//             $('.topBtn').fadeIn('slow');  //top보임
+//         }else{
+//             $('.topBtn').fadeOut('fast');//top안보임
+//         }
+//     });
 
-
-
+    $('.topBtn').click(function(e){
+        e.preventDefault();
+        //상단으로 스르륵 이동합니다.
+        $("html,body").stop().animate({"scrollTop":0},1000); //요거 중요 {"scrollTop":100 -> (top좌표만큼 이동)},1000
+    });
 
 
 // modal js
